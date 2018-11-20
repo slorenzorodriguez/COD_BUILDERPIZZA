679ef0baf772e7ab0dfb125bf0c561907ed950a0
slorenzorodriguez <slorenzorodriguez@nautilus22.danielcastelao.org>
2018-11-20 09:29:23 +0100

    rama feature

--- FastForward/src/fastforward/FastForward.java
+++ FastForward/src/fastforward/FastForward.java
@@ -15,7 +15,7 @@ public class FastForward {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //rama master
+        //rama feature
         
     }
     
